0d0026aa63ab6c8188283879eed8bbeb024eb458
PackWeb <info@packweb.eu>
2020-05-25 12:25:58 +0300

    Added layouts and themes to setup, and fixed issue #1.

--- scripts/setup.php
+++ scripts/setup.php
@@ -26,7 +26,6 @@ foreach ($projects['modules']['contrib'] as $module => $data) {
     $modules['git'][$module] = $data['#git'];
   }
 }
-
 // Download modules.
 foreach ($modules['dl'] as $module) {
   if (!file_exists($root . '/modules/' . $module)) {
@@ -37,6 +36,58 @@ foreach ($modules['dl'] as $module) {
 foreach ($modules['git'] as $module => $repo) {
   if (!file_exists($root . '/modules/' . $module)) {
     echo "Cloning $module\n";
+    exec('git clone ' . $repo);
+  }
+}
+
+// Get list of layouts.
+$layouts['dl'] = array_keys($projects['layouts']);
+foreach ($projects['layouts'] as $layout => $data) {
+  if (isset($data['#git'])) {
+    // Remove from download list.
+    $key = array_search($layout, $layouts['dl']);
+    unset($layouts['dl'][$key]);
+
+    // Add to git list.
+    $layouts['git'][$layout] = $data['#git'];
+  }
+}
+// Download layouts.
+foreach ($layouts['dl'] as $layout) {
+  if (!file_exists($root . '/layouts/' . $layout)) {
+    echo "Downloading $layout\n";
+    exec('b dl -y ' . $layout);
+  }
+}
+foreach ($layouts['git'] as $layout => $repo) {
+  if (!file_exists($root . '/layouts/' . $layout)) {
+    echo "Cloning $layout\n";
+    exec('git clone ' . $repo);
+  }
+}
+
+// Get list of themes.
+$themes['dl'] = array_keys($projects['themes']);
+foreach ($projects['themes'] as $theme => $data) {
+  if (isset($data['#git'])) {
+    // Remove from download list.
+    $key = array_search($theme, $themes['dl']);
+    unset($themes['dl'][$key]);
+
+    // Add to git list.
+    $themes['git'][$theme] = $data['#git'];
+  }
+}
+// Download themes.
+foreach ($themes['dl'] as $theme) {
+  if (!file_exists($root . '/themes/' . $theme)) {
+    echo "Downloading $theme\n";
+    exec('b dl -y ' . $theme);
+  }
+}
+foreach ($themes['git'] as $theme => $repo) {
+  if (!file_exists($root . '/themes/' . $theme)) {
+    echo "Cloning $theme\n";
     exec('git clone ' . $repo);
   }
 }
