00e4d275182584fea2bc04bb3a5bb3b3167d5997
PackWeb <info@packweb.eu>
2020-05-27 23:09:19 +0300

    Started updating settings to work properly.

--- scripts/setup.php
+++ scripts/setup.php
@@ -10,23 +10,17 @@
  */
 
 $root = __DIR__ . '/../../..';
-
-// Get the list of projects.
 require_once __DIR__ . '/../includes/projects.inc';
 
-// Get list of contrib modules.
+// Modules.
 $modules['dl'] = array_keys($projects['modules']['contrib']);
 foreach ($projects['modules']['contrib'] as $module => $data) {
   if (isset($data['#git'])) {
-    // Remove from download list.
     $key = array_search($module, $modules['dl']);
     unset($modules['dl'][$key]);
-
-    // Add to git list.
     $modules['git'][$module] = $data['#git'];
   }
 }
-// Download modules.
 foreach ($modules['dl'] as $module) {
   if (!file_exists($root . '/modules/' . $module)) {
     echo "Downloading $module\n";
@@ -40,19 +34,15 @@ foreach ($modules['git'] as $module => $repo) {
   }
 }
 
-// Get list of layouts.
+// Layouts.
 $layouts['dl'] = array_keys($projects['layouts']);
 foreach ($projects['layouts'] as $layout => $data) {
   if (isset($data['#git'])) {
-    // Remove from download list.
     $key = array_search($layout, $layouts['dl']);
     unset($layouts['dl'][$key]);
-
-    // Add to git list.
     $layouts['git'][$layout] = $data['#git'];
   }
 }
-// Download layouts.
 foreach ($layouts['dl'] as $layout) {
   if (!file_exists($root . '/layouts/' . $layout)) {
     echo "Downloading $layout\n";
@@ -66,19 +56,15 @@ foreach ($layouts['git'] as $layout => $repo) {
   }
 }
 
-// Get list of themes.
+// Themes.
 $themes['dl'] = array_keys($projects['themes']);
 foreach ($projects['themes'] as $theme => $data) {
   if (isset($data['#git'])) {
-    // Remove from download list.
     $key = array_search($theme, $themes['dl']);
     unset($themes['dl'][$key]);
-
-    // Add to git list.
     $themes['git'][$theme] = $data['#git'];
   }
 }
-// Download themes.
 foreach ($themes['dl'] as $theme) {
   if (!file_exists($root . '/themes/' . $theme)) {
     echo "Downloading $theme\n";
